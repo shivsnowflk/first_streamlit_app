1275a81a0589118c4fa3ee8d68a1a2e3b0b830ac
shivsnowflk <105466356+shivsnowflk@users.noreply.github.com>
2022-05-14 00:11:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🍌🍉🍎🍐🍓 Fruit salad')
 
 streamlit.header('🍌🥭 Build your own smoothie 🥝🍇')
 
